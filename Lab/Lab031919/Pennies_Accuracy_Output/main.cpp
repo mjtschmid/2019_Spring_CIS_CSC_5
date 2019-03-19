9c24712ec022156093a02d9fa40bbcfed8640866
ml1150258 <mark.lehr@rcc.edu>
2019-03-19 12:40:10 -0700

    Signed-off-by: ml1150258 <mark.lehr@rcc.edu>

---
+++ Lab/Lab031919/Pennies_Accuracy_Output/main.cpp
@@ -0,0 +1,49 @@
+/* 
+ * File:   main.cpp
+ * Author: Dr. Mark E. Lehr
+ * Created on March 19th, 2019, 12:26 PM
+ * Purpose:  Salary starting at a penny
+ */
+
+//System Libraries
+#include <iostream>  //Input/Output Library
+#include <iomanip>   //Format Library
+using namespace std;
+
+//User Libraries
+
+//Global Constants, no Global Variables are allowed
+//Math/Physics/Conversions/Higher Dimensions - i.e. PI, e, etc...
+const int CNVPNDL=100;//Convert Pennies to Dollars
+
+//Function Prototypes
+
+//Execution Begins Here!
+int main(int argc, char** argv) {
+    //Declare Variables
+    
+    //Initialize or input i.e. set variable values
+    cout<<"This program Calculates pay per day and sum"<<endl;
+    cout<<"Day         Pay          Sum"<<endl;
+    
+    //Map inputs -> outputs
+    cout<<fixed<<setprecision(2)<<showpoint;
+    for(unsigned int day=1,pay=1,sum=0;day<=31;day++,pay*=2){
+        sum+=pay;
+        //Print Dollars then Pennies
+        cout<<setw(3)<<day
+            <<setw(9)<<(pay/CNVPNDL)<<".";
+            if(pay%CNVPNDL<10){
+                cout<<'0';
+            }
+            cout<<pay%CNVPNDL
+            <<setw(10)<<(sum/CNVPNDL)<<".";
+            if(sum%CNVPNDL<10){
+                cout<<'0';
+            }
+            cout<<sum%CNVPNDL<<endl;
+    }
+    
+    //Exit stage right or left!
+    return 0;
+}
